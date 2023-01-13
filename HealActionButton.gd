fcadd2f42e48bf79ac0e06115ca439f1d1eb3f85
Paulo Barreto <paulovgtb@gmail.com>
2023-01-13 12:15:58 +0000

    Creates hover info scene and adds to the enemy and action button scene

--- HealActionButton.gd
+++ HealActionButton.gd
@@ -2,7 +2,7 @@ extends "res://ActionButton.gd"
 
 func _on_pressed():
 	var playerStats = BattleUnits.PlayerStats
-	if playerStats != null && playerStats.mp >= 8:
+	if playerStats != null and playerStats.mp >= 8:
 		playerStats.hp += 5
 		playerStats.mp -= 8
 		playerStats.ap -= 1
---
+++ HoverInfo.gd
@@ -0,0 +1,9 @@
+extends Control
+
+export (String, MULTILINE) var description
+
+func _on_HoverInfo_mouse_entered():
+	print(description)
+
+func _on_HoverInfo_mouse_exited():
+	pass # Replace with function body.
